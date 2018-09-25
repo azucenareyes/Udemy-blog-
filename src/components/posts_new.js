41ee002392aec5dec3de79d6601d434ca98f175f
Azucena Martinez <azucena_rys@yahoo.com>
2018-09-25 12:59:19 -0600

    syntax error

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Field, reduxForm } from 'redux-Form'
+import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { createPost } from '../actions/index'
